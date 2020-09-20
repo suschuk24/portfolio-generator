701af05c73d58734cbed8b5403575684509730b4
Seth Uschuk <suschuk24@gmail.com>
2020-09-20 13:01:35 -0500

    give user option to save about me info

--- app.js
+++ app.js
@@ -42,9 +42,16 @@ inquirer
         }
       },
       {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
         type: 'input',
         name: 'about',
-        message: 'Provide some informaiton about yourself'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => confirmAbout
       },
     ]);
   };
