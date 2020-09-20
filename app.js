fa23c53f7c404aa94f5b9b556dc33b729c767f73
Seth Uschuk <suschuk24@gmail.com>
2020-09-20 10:22:02 -0500

    created js code to create array for command line input

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+console.log(profileDataArgs)
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
-  
-  printProfileData(profileDataArgs);
+const printProfileData = profileDataArr => {
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+  console.log('================');
+
+profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
